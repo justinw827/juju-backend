648288103aaefe17f4dce004337f20caca33499a
Justin Wang <justinw0827@gmail.com>
2018-11-06 16:39:26 -0500

    added search and queue handling

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -17,7 +17,6 @@ class Api::V1::EventsController < ApplicationController
       Authorization: "Bearer #{user.access_token}"
     }
 
-    byebug
     # Temporary hard coded playlist id
     new_event = Event.create!(name: event_name, description: event_description, group_id: 1, playlist_id: "3qBswSHe4Hhjux8tpB4qGE")
 
@@ -25,6 +24,12 @@ class Api::V1::EventsController < ApplicationController
 
     # spotify_response = RestClient.post('https://api.spotify.com/v1/playlists', postBody.to_json, headers=postHeaders)
     render json: { event: EventSerializer.new(new_event) }, status: :ok
+  end
+
+  def show
+    @party = Event.find(params[:id])
+
+    render json: { party: EventSerializer.new(@party) }, status: :ok
   end
 
   def get_all_events
@@ -35,5 +40,23 @@ class Api::V1::EventsController < ApplicationController
     end
 
     render json: @newEvents, status: :ok
+  end
+
+  def add_song
+    @party = Event.find(params[:id])
+
+    access_token = "BQCatGEW_BOpQstkgY2iU_2iIfUxpag1ZO4ABQqjqcbL7HWdjVOw8W3i681yb96TIm5hq5SPEo0K16iGAiwcMKUOcB_Xr-bXleDeHxcs8NYbg9t4hIQe4e8GnND5AP5HNR57gOKmCirmTLcVPJ72zszjFtFK4bjXN6o529fSJlPMjlOTU6tNtTvKbUUeyAqHXH2LHFGLsmQ-0E9yqmoyQwakUwl10RNPfjiHdVC0ZIG2cnnlDrGXRnxLSEfWHQpnNkWs2qrqOAifLRDNURc"
+
+    header = {
+      Authorization: "Bearer #{access_token}"
+    }
+
+    url = {
+      uris: params["url"]
+    }
+    endpoint = "https://api.spotify.com/v1/playlists/#{@party.playlist_id}/tracks?#{url.to_query}"
+    post_response = RestClient.post(endpoint, {}, header)
+
+    render json: @party, status: :ok
   end
 end
